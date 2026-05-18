659f9c496ad2108d1987d34bb1faf4429b5dfc81
cristichiu <grajdiancristian@gmail.com>
2026-05-18 08:58:30 +0300

    delete presentations

--- slides/09-metode-dezvoltare-uml/setup/main.ts
+++
@@ -1,9 +0,0 @@
-import { defineAppSetup } from '@slidev/types'
-import sharedSetup from '../../../common/setup/main'
-import '../../../common/theme/style.css'
-
-export default defineAppSetup(({ app }) => {
-  if (sharedSetup && typeof sharedSetup === 'function') {
-    sharedSetup({ app } as any)
-  }
-})
